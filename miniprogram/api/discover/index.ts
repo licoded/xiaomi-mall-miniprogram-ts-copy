163161e3df88e42d086077880d074942ada6d1d5
licoded <busy.li@foxmail.com>
2023-04-24 08:58:55 +0800

    api: add getProductByCategory API

--- miniprogram/api/discover/index.ts
+++ miniprogram/api/discover/index.ts
@@ -1,12 +1,29 @@
 import { post } from '../../utils/request';
 import { ICategory } from '../../types/category';
+import { IProduct } from '../../types/product';
 
 interface IGetcategoryListResp { 
   code: number;
   category: ICategory[];
 }
 
+interface IGetProductByCategoryResp { 
+  code: number;
+  Product: IProduct[];
+}
+
+interface categoryIDPageParams {
+  pageSize: number;
+  currentPage: number;
+  categoryID: number[];
+}
+
 export async function getCategoryList() {
   const categoryListResp = await post<{}, IGetcategoryListResp>('/product/getCategory', {});
   return categoryListResp.category;
+}
+
+export async function getProductByCategory(params: categoryIDPageParams) {
+  const categoryListResp = await post<{}, IGetProductByCategoryResp>('/product/getProductByCategory', params);
+  return categoryListResp.Product;
 }
---
+++ miniprogram/types/product.ts
@@ -0,0 +1,12 @@
+export interface IProduct {
+  category_id: number;
+  product_id: number;
+  product_intro: string;
+  product_name: string;
+  product_num: number;
+  product_picture: string;
+  product_price: number;
+  product_sales: number;
+  product_selling_price: number;
+  product_title: string;
+}

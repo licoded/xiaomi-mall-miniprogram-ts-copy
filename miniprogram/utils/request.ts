f87bb85324fe9e27a0cae86d4752e3043269ae35
licoded <busy.li@foxmail.com>
2023-04-24 11:53:58 +0800

    request-config: add Request Timeout Limit

--- miniprogram/utils/request.ts
+++ miniprogram/utils/request.ts
@@ -1,5 +1,10 @@
 export const baseURL = "https://licoded.site";
 
+// reference: https://www.cnblogs.com/apple78/p/13463172.html
+function sleep(time: number) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
+
 function request<IParams, IResponse>(
   api: string,
   params: IParams,
@@ -7,6 +12,18 @@ function request<IParams, IResponse>(
 ): Promise<IResponse> {
   const url = baseURL + api;
   return new Promise((resolve) => {
+    let finishFlag = false;
+    // set Request Timeout Limit
+    setTimeout(() => {
+      if(finishFlag) return ;
+      finishFlag = true;
+      wx.hideLoading();
+      wx.showModal({
+        title: 'Request Timeout!',
+      });
+      console.error('Request Timeout!');
+    }, 80+Math.random()*100)
+
     // open/set 'loading' modal
     wx.showLoading({
       title: 'loading...',
@@ -18,7 +35,9 @@ function request<IParams, IResponse>(
       url,
       method: type,
       data: params,
-      success: function(res) {
+      success: async function(res) {
+        if(finishFlag) return;
+        finishFlag = true;
         resolve(res.data as IResponse);
       },
       fail: function(error) {

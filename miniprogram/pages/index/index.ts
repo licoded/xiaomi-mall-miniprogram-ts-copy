6a8da8b01fe1a25896afff6d497b73d0db0344cb
licoded <busy.li@foxmail.com>
2023-04-24 09:18:14 +0800

    pages/index: fetch CategoryList and show

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -1,9 +1,13 @@
 // index.ts
+import type { ICategory } from '../../types/category';
+import { getCategoryList } from '../../api/discover/index';
 
 Page({
   data: {
-    
+    categoryList: [] as ICategory[],
   },
-  onLoad() {
+  async onLoad() {
+    const categoryList = await getCategoryList();
+    this.setData({ categoryList });
   },
 })
